e8d20570f9ad313607ba51c0164e7461265dedc7
ff-ondics <fink@ondics.de>
2025-06-10 12:58:49 +0200

    refactor

--- tests/test.py
+++ tests/test.py
@@ -2,21 +2,25 @@ from agents import Agent, Runner, trace
 from agents.mcp import MCPServer, MCPServerStdio
 import asyncio
 import shutil
+from dotenv import load_dotenv
 
-async def run(mcp_server: MCPServer, directory_path: str):
+
+load_dotenv()
+
+async def run(mcp_server: MCPServer):
     agent = Agent(
         name="Assistant",
-        instructions=f"Answer questions about the git repository at {directory_path}, use that for repo_path",
+        instructions=f"Answer questions about the ckan site",
         mcp_servers=[mcp_server],
     )
 
-    message = "Who's the most frequent contributor?"
+    message = "Who created the most Datasets?"
     print("\n" + "-" * 40)
     print(f"Running: {message}")
     result = await Runner.run(starting_agent=agent, input=message)
     print(result.final_output)
 
-    message = "Summarize the last change in the repository."
+    message = "which is the newest Dataset?."
     print("\n" + "-" * 40)
     print(f"Running: {message}")
     result = await Runner.run(starting_agent=agent, input=message)
@@ -24,14 +28,14 @@ async def run(mcp_server: MCPServer, directory_path: str):
 
 async def main():
     # Ask the user for the directory path
-    directory_path = input("Please enter the path to the git repository: ")
+    # directory_path = input("Please enter the path to the git repository: ")
 
     async with MCPServerStdio(
         cache_tools_list=True,  # Cache the tools list, for demonstration
-        params={"command": "uvx", "args": ["mcp-server-git"]},
+        params={"command": "uv", "args": ["run","mcp_ckan_server.py"],"cwd":".."},
     ) as server:
         with trace(workflow_name="MCP Git Example"):
-            await run(server, directory_path)
+            await run(server)
 
 
 if __name__ == "__main__":
